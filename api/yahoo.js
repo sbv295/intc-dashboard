6e867505851ebef66fd053f0922d654b258ee041
swarvaid <swarvaid@intel.com>
2026-06-23 15:18:38 +0530

    Add watchlist page with 10 stocks, logos, brand colors, live NASDAQ + HL prices

--- api/yahoo.js
+++ api/yahoo.js
@@ -1,13 +1,14 @@
 export default async function handler(req, res) {
-  const { interval, range, period1, period2, includePrePost, modules } = req.query;
+  const { interval, range, period1, period2, includePrePost, modules, symbol } = req.query;
+  const ticker = symbol || 'INTC';
 
   let url;
   if (modules) {
     // quoteSummary endpoint
-    url = `https://query1.finance.yahoo.com/v10/finance/quoteSummary/INTC?modules=${modules}`;
+    url = `https://query1.finance.yahoo.com/v10/finance/quoteSummary/${ticker}?modules=${modules}`;
   } else {
     // chart endpoint
-    url = `https://query1.finance.yahoo.com/v8/finance/chart/INTC?interval=${interval || '5m'}`;
+    url = `https://query1.finance.yahoo.com/v8/finance/chart/${ticker}?interval=${interval || '5m'}`;
     if (period1 && period2) {
       url += `&period1=${period1}&period2=${period2}`;
     } else {
